9258b0703341d582c1ae6693356bc0d50ecc6bf6
hafez <elahwanyhafez@gmail.com>
2024-01-04 23:21:32 -0700

    add add note cubit to add note button sheet

--- lib/cubites/add_note_cubit/add_note_cubit_cubit.dart
+++ lib/cubites/add_note_cubit/add_note_cubit_cubit.dart
@@ -1,8 +1,22 @@
 import 'package:bloc/bloc.dart';
+import 'package:hive_flutter/adapters.dart';
 import 'package:meta/meta.dart';
+import 'package:note_app/models/note_model.dart';
+import 'package:note_app/widgets/constants.dart';
 
 part 'add_note_cubit_state.dart';
 
-class AddNoteCubitCubit extends Cubit<AddNoteCubitState> {
-  AddNoteCubitCubit() : super(AddNoteCubitInitial());
+class AddNoteCubit extends Cubit<AddNoteCubitState> {
+  AddNoteCubit() : super(AddNoteCubitInitial());
+
+  addNote(NoteModel note) async {
+    emit(AddNoteCubitLoading());
+    try {
+      var notesBox = Hive.box<NoteModel>(kNotesBox);
+      emit(AddNoteCubitSucsses());
+      await notesBox.add(note);
+    } catch (e) {
+      AddNoteCubitFailure(e.toString());
+    }
+  }
 }
